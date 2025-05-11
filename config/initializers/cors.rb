838cae777013a73f5c5c0c0e5a4b9953259bdffc
Shanhun-Codes <shanhun.codes@gmail.com>
2025-05-11 00:38:17 -0500

    CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "https://applyiq.netlify.app" || "http://localhost:4200"
     resource "*",
       headers: :any,
+      expose: [ "Authorization" ],
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
   end
